a698ef6763db60a27c205b6fb5f8d99cb371ef1d
jackyeneas <58607680+jackyeneas@users.noreply.github.com>
2022-11-16 11:50:46 -0300

    Add files via upload

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,13 +1,12 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Page = () => {
     return (
         <div>
-            <h2>Página Sobre</h2>
+            <h2>About</h2>
             <Link to='/'>Home</Link>
         </div>
     )
 }
-
 export default Page
