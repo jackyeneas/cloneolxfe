2ebb794a2f2eb80848b0f38418dd69acb0c2737d
jackyeneas <58607680+jackyeneas@users.noreply.github.com>
2022-11-16 11:45:52 -0300

    Add files via upload

--- src/helpers/OlxAPI.js
+++ src/helpers/OlxAPI.js
@@ -1,7 +1,7 @@
-import Cookies from 'js-cookie';
+import Cookies from  'js-cookie';
 import qs from 'qs';
 
-const BASEAPI = '';
+const BASE_API = 'http://localhost:6060';
 
 const apiFetchFile = async (endpoint, body) => {
     if(!body.token) {
@@ -10,7 +10,7 @@ const apiFetchFile = async (endpoint, body) => {
             body.append('token', token);
         }
     }
-    const res = await fetch(BASEAPI + endpoint, {
+    const res = await fetch(BASE_API + endpoint, {
         method: 'POST',
         body
     });
@@ -22,54 +22,82 @@ const apiFetchFile = async (endpoint, body) => {
     return json;
 }
 
+const apiFetchPut = async (endpoint, body) => {
+    if(!body.token) {
+        let token = Cookies.get('token');
+        if(token){
+            body.token = token;
+        }
+    }
+    
+    const res = await fetch(BASE_API + endpoint, {
+        method: 'PUT',
+        headers: {
+            'Accept':'application/json',
+            'Content-type':'application/json'
+        },
+        body: JSON.stringify(body),
+    });
+
+    const json = await res.json();
+    if(json.notallowed){
+        window.location.href = '/signin';
+        return;
+    }
+
+    return json;
+}
 
 const apiFetchPost = async (endpoint, body) => {
     if(!body.token) {
         let token = Cookies.get('token');
-        if (token) {
+        if(token){
             body.token = token;
         }
     }
-    const res = await fetch(BASEAPI + endpoint, {
+    const res = await fetch(BASE_API + endpoint, {
         method: 'POST',
         headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
+            'Accept':'application/json',
+            'Content-type':'application/json'
         },
         body: JSON.stringify(body)
     });
+
     const json = await res.json();
-    if(json.notallowed) {
+    if(json.notallowed){
         window.location.href = '/signin';
         return;
     }
+
     return json;
 }
 
 const apiFetchGet = async (endpoint, body = []) => {
     if(!body.token) {
         let token = Cookies.get('token');
-        if (token) {
+        if(token){
             body.token = token;
         }
     }
-    const res = await fetch(`${BASEAPI + endpoint}?${qs.stringify(body)}`);
+    const res = await fetch(`${BASE_API+endpoint}?${qs.stringify(body)}`);
+
     const json = await res.json();
-    if(json.notallowed) {
+    if(json.notallowed){
         window.location.href = '/signin';
         return;
     }
+
     return json;
 }
 
 const OlxAPI = {
     login: async (email, password) => {
-        const json = await apiFetchPost('user/signin', { email, password });
+        const json = await apiFetchPost('/user/login', {email, password});
         return json;
     },
-
     register: async (name, stateLoc, email, password) => {
-        const json = await apiFetchPost('user/signup', {
+        const json = await apiFetchPost('/user/signup', {
             name,
             state: stateLoc,
             email,
@@ -77,7 +105,6 @@ const OlxAPI = {
         });
         return json;
     },
-
     getState: async () => {
         const json = await apiFetchGet('/states');
         return json.states;
@@ -112,7 +139,31 @@ const OlxAPI = {
             fData
         );
         return json;
-    }
+    },
+
+    updateAd: async (id, fData) => {
+        const json = await apiFetchFile(
+            `/ad/${id}`
+        );
+        return json;
+    },
+
+    getUser: async () => {
+        const json = await apiFetchGet(
+            '/user/me'
+        );
+        return json;
+    },
+
+    updateUser: async (name, stateLoc, email, password) => {
+        const json = await apiFetchPut('/user/me', {
+            name,
+            state: stateLoc,
+            email,
+            password
+        });
+        return json;
+    },
 }
 
 export default () => OlxAPI;
