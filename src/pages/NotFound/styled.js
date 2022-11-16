2ce5b0b54452138375057c0603bc0ee0dfc390fe
jackyeneas <58607680+jackyeneas@users.noreply.github.com>
2022-11-16 11:52:04 -0300

    Add files via upload

--- src/pages/NotFound/styled.js
+++ src/pages/NotFound/styled.js
@@ -1,37 +1,35 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const NotFoundArea = styled.div`
 background-color: #999;
 
-.conteudo {
+
+.conteudo{
     display: flex;
     height: 400px;
 
-    .botao {
-        width: 200px;
+    .botao{
+        width: 100%;
         height: 50px;
         line-height: 30px;
         display: flex;
-        margin-top: 300px;
-        margin-right: 30px;
-        margin-left: auto;
+        margin: 250px 30px 0 auto;
 
-        a {
+        a{
             text-decoration: none;
             padding: 10px 20px;
             background-color: #BA55D3;
             border-radius: 5px;
             border: 1px solid #4B0082;
-            color: #FFF;
             transition: all ease 0.4s;
+        }
 
-            &:hover {
-                background-color: #4B0082;
-                border: 1px solid #BA55D3;
-            }
+        a:hover{
+            background-color: #4B0082;
+            border: 1px solid #BA55D3;
+            color: #fff;
         }
 
     }
 }
-
 `
