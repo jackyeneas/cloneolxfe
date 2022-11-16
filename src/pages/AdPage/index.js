da9ca9ece9a6468e8a7eb5b2d86a3544bd7f40e5
jackyeneas <58607680+jackyeneas@users.noreply.github.com>
2022-11-16 11:51:04 -0300

    Add files via upload

--- src/pages/AdPage/index.js
+++ src/pages/AdPage/index.js
@@ -1,57 +1,37 @@
-import 
-React, 
-{ 
-    useState, 
-    useEffect 
-} from 'react';
-import 
-{ 
-    PageArea,
-    Fake,
-    OthersArea,
-    BreadChump
-} from './styled';
+import React, {useState, useEffect} from "react";
 import { 
     useParams,
     Link
 } from 'react-router-dom';
-import { Slide } from 'react-slideshow-image';
+import { Slide } from "react-slideshow-image";
 import 'react-slideshow-image/dist/styles.css';
-import { PageContainer } from '../../components/MainComponents';
-import AdItem from '../../components/partials/AdItem';
+import { PageArea, Fake, OthersArea, BreadChumb } from "./styled";
+import { PageContainer } from "../../components/MainComponents";
 import useApi from '../../helpers/OlxAPI';
+import AdItem from '../../components/partials/AdItem';
 
 const Page = () => {
     const api = useApi();
     const { id } = useParams();
+    const [ loading, setLoading] = useState(true);
+    const [ adInfo, setAdInfo] = useState({});
 
-    const [loading, setLoading] = useState(true);
-    const [adInfo, setAdInfo] = useState({});
-
-    useEffect(() => {
+    useEffect(()=>{
         const getAdInfo = async (id) => {
-            const json = await api.getAd(id, true);
+            const json = await api.getAd(id,true);
             setAdInfo(json);
             setLoading(false);
         }
+
         getAdInfo(id);
     }, []);
 
     const formatDate = (date) => {
         let cDate = new Date();
         let months = [
-            'janeiro',
-            'fevereiro',
-            'março',
-            'abril',
-            'maio',
-            'junho',
-            'julho',
-            'agosto',
-            'setembro',
-            'outubro',
-            'novembro',
-            'dezembro'
+            'janeiro', 'fevereiro', 'março', 'abril',
+            'junho', 'julho', 'agosto', 'setembro',
+            'outubro', 'novembro', 'dezembro'
         ];
         let cDay = cDate.getDate();
         let cMonth = cDate.getMonth();
@@ -62,40 +42,32 @@ const Page = () => {
     return (
         <PageContainer>
             {adInfo.category &&
-                <BreadChump>
+                <BreadChumb>
                     Você está em:
-                    <Link
-                        to="/"
-                    >
+                    <Link to="/" >
                         Home
                     </Link>
                     /
-                    <Link
-                        to={`/ads?state=${adInfo.stateName}`}
-                    >
+                    <Link to={`/ads?state=${adInfo.stateName}`} >
                         {adInfo.stateName}
                     </Link>
                     /
-                    <Link
-                        to={`/ads?state=${adInfo.stateName}&cat=${adInfo.category.slug}`}
-                    >
+                    <Link to={`/ads?state=${adInfo.stateName}&cat=${adInfo.category.slug}`} >
                         {adInfo.category.name}
                     </Link>
                     / {adInfo.title}
-                </BreadChump>
+                </BreadChumb>
             }
             <PageArea>
                 <div className="leftSide">
                     <div className="box">
                         <div className="adImage">
-                            {loading && 
-                                <Fake height={300} />
-                            }
+                            {loading && <Fake height={300}/> }
                             {adInfo.images &&
                                 <Slide>
-                                    {adInfo.images.map((img, k) =>
-                                        <div key={k} className="each-slide">
-                                            <img src={img} alt="" />
+                                    {adInfo.images.map((image, key)=>
+                                        <div key={key} className="each-slide">
+                                            <img src={image} alt=""/>
                                         </div>
                                     )}
                                 </Slide>
@@ -103,24 +75,18 @@ const Page = () => {
                         </div>
                         <div className="adInfo">
                             <div className="adName">
-                                {loading && <Fake height={20} />}
-                                {adInfo.title &&
-                                    <h2>{adInfo.title}</h2>
-                                }
-                                {adInfo.dateCreated &&
-                                    <small>
-                                        Criado em: {formatDate(adInfo.dateCreated)}
-                                    </small>
+                                {loading &&  <Fake height={20}/> }
+                                {adInfo.title &&  <h2>{adInfo.title}</h2> }
+                                {adInfo.dateCreated &&  
+                                    <small> Criado em: {formatDate(adInfo.dateCreated)}</small> 
                                 }
                             </div>
                             <div className="adDescription">
-                                {loading && <Fake height={100} />}
+                                {loading && <Fake height={100}/> }
                                 {adInfo.description}
-                                <hr />
+                                <hr/>
                                 {adInfo.views &&
-                                    <small>
-                                        Visualizações: {adInfo.views}
-                                    </small>
+                                    <small>Visualizações: {adInfo.views}</small>
                                 }
                             </div>
                         </div>
@@ -128,7 +94,7 @@ const Page = () => {
                 </div>
                 <div className="rightSide">
                     <div className="box box--padding">
-                        {loading && <Fake height={20} />}
+                        {loading && <Fake height={20}/> }
                         {adInfo.priceNegotiabled && 
                             "Preço Negociável"
                         }
@@ -138,24 +104,20 @@ const Page = () => {
                             </div>
                         }
                     </div>
-                    {loading && <Fake height={50} />}
-                    {adInfo.userInfo && 
+                    {loading && <Fake height={50}/>}
+                    {adInfo.userInfo &&
                         <>
-                            <a
-                                href={`mailto:${adInfo.userInfo.email}`}
+                             <a href={`mailto:${adInfo.userInfo.email}`}
                                 target="_blank"
                                 className="contactSellerLink"
-                            >
-                                Fale com o vendedor
-                            </a>
-                            <div className=" createBy box box--padding">
+                            > Fale com o vendedor </a>
+                            <div className="createBy box box--padding">
                                 <strong>{adInfo.userInfo.name}</strong>
                                 <small>Email: {adInfo.userInfo.email}</small>
                                 <small>Estado: {adInfo.userInfo.stateName}</small>
                             </div>
                         </>
                     }
-                    
                 </div>
             </PageArea>
             <OthersArea>
@@ -163,15 +125,16 @@ const Page = () => {
                     <>
                         <h2>Outras ofertas do vendedor</h2>
                         <div className="list">
-                            {adInfo.others.map((i,k) => 
-                                <AdItem key={k} data={i} />
-                            )}    
-                        </div>    
+                            {adInfo.others.map((item,key)=>
+                                <AdItem key={key} data={item}/>
+                            )}
+                        </div>
                     </>
                 }
             </OthersArea>
         </PageContainer>
-    )
+        
+    );
 }
 
 export default Page;
--- src/pages/AdPage/styled.js
+++ src/pages/AdPage/styled.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Fake = styled.div`
     background-color: #ddd;
@@ -9,31 +9,31 @@ export const PageArea = styled.div`
     display: flex;
     margin-top: 20px;
 
-    .box {
+    .box{
         background-color: #fff;
         border-radius: 5px;
-        box-shadow: 0px 0px 4px #999;
+        box-shadow: 0 0 4px #999;
         margin-bottom: 20px;
     }
 
-    .box--padding {
-        padding: 10px;
+    .box--padding{
+        padding:10px;
     }
 
     .leftSide {
         flex: 1;
         margin-right: 20px;
 
-        .box {
+        .box{
             display: flex;
         }
 
-        .adImage {
+        .adImage{
             width: 320px;
             height: 320px;
-            margin-right: 20px;
+            margin-right:20px;
 
-            .each-slide {
+            .each-slide{
                 display: flex;
                 align-items: center;
                 justify-content: center;
@@ -42,44 +42,42 @@ export const PageArea = styled.div`
             }
         }
 
-        .adInfo {
+        .adInfo{
             padding: 10px;
             flex: 1;
 
-            .adName {
+            .adName{
                 margin-bottom: 10px;
 
-                h2 {
-                    margin: 0;
-                    margin-top: 20px;
+                h2{
+                    margin: 20px 0 0 0;
                 }
 
-                small {
+                small{
                     color: #999;
                 }
             }
 
-            .adDescription {
-
-                small {
+            .adDescription{
+                small{
                     color: #999;
                 }
             }
         }
     }
 
-    .rightSide {
+    .rightSide{
         width: 250px;
 
-        .price span {
-            color: #0000ff;
+        .price span{
+            color:  #0000ff;
             display: block;
             font-size: 27px;
             font-weight: bold;
         }
 
         .contactSellerLink {
-            background-color: #0000ff;
+            background-color:  #0000ff;
             color: #fff;
             height: 30px;
             border-radius: 5px;
@@ -94,8 +92,7 @@ export const PageArea = styled.div`
         .createBy strong {
             display: block;
         }
-
-        .createBy small {
+        .createBy small{
             display: block;
             color: #999;
             margin-top: 10px;
@@ -104,14 +101,14 @@ export const PageArea = styled.div`
 `;
 
 export const OthersArea = styled.div`
-    h2 {
+    h2{
         font-size: 20px;
     }
 
-    .list {
+    .list{
         display: flex;
 
-        .aditem {
+        .adItem {
             width: 25%;
         }
     }
@@ -120,7 +117,6 @@ export const OthersArea = styled.div`
 export const BreadChumb = styled.div`
     font-size: 13px;
     margin-top: 20px;
-
     a {
         display: inline-block;
         margin: 0 5px;
