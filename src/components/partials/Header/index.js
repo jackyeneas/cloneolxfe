9d0b10670cd933ba9fcf371934ee2a8e7e2b3a51
jackyeneas <58607680+jackyeneas@users.noreply.github.com>
2022-11-16 11:47:18 -0300

    Add files via upload

--- src/components/partials/Header/index.js
+++ src/components/partials/Header/index.js
@@ -1,54 +1,60 @@
-import React from 'react'
+import React from "react";
 import { HeaderArea } from './styled'
-import { Link } from 'react-router-dom'
-import { isLogged } from '../../../helpers/AuthHandler'
+import { Link } from "react-router-dom";
+import { isLogged, doLogout } from '../../../helpers/AuthHandler'
 
 const Header = () => {
-	let logged = isLogged()
+    let logged = isLogged();
 
-	return (
-		<HeaderArea>
-			<div className="container">
-				<div className="logo">
-					<Link to='/'>
-						<spam className="logo-1">O</spam>
-						<spam className="logo-2">L</spam>
-						<spam className="logo-3">X</spam>
-					</Link>
-				</div>
-				<nav>
-					<ul>
-						{!logged &&
-							<>
-								<li>
-									<Link to="/signin">Login</Link>
-								</li>
-								<li>
-									<Link to="/signup">Cadatrar</Link>
-								</li>
-								<li>
-									<Link to="/signin" className="button">Postar um anúncio</Link>
-								</li>
-							</>
-						}
-						{logged &&
-							<>
-								<li>
-									<Link to="/my-account">Minha Conta</Link>
-								</li>
-								<li>
-									<Link to="/logout">Sair</Link>
-								</li>
-								<li>
-									<Link to="/post-an-ad" className="button">Postar um anúncio</Link>
-								</li>
-							</>
-						}
-					</ul>
-				</nav>
-			</div>
-		</HeaderArea>
-	)
+    const logout = () => {
+        doLogout();
+        window.location.href = '/';
+    }
+    return (
+        <HeaderArea>
+            <div className="container">
+                <div className="logo">
+                    <Link to="/">
+                        <span className="logo-1">O</span>
+                        <span className="logo-2">L</span>
+                        <span className="logo-3">X</span>
+                    </Link>
+                </div>
+                <nav>
+                    <ul>
+                        {!logged && 
+                            <>
+                                <li>
+                                    <Link to="/signin">Login</Link>
+                                </li>
+                                <li>
+                                    <Link to="/signup">Cadastrar</Link>
+                                </li>
+                                <li>
+                                    <Link to="/signin" className="button">
+                                        Postar um Anúncio</Link>
+                                </li>
+                            </>
+                        }
+                        {logged && 
+                            <>
+                                <li>
+                                    <Link to="/my-account">Minha Conta</Link>
+                                </li>
+                                <li>
+                                    <Link to="/" onClick={()=>logout()}>Sair</Link>
+                                </li>
+                                <li>
+                                    <Link to="/post-an-ad" className="button">
+                                        Postar um Anúncio</Link>
+                                </li>
+                            </>
+                        }
+                    </ul>
+                </nav>
+            </div>
+        </HeaderArea>
+    )
 }
 
 export default Header
--- src/components/partials/Header/styled.js
+++ src/components/partials/Header/styled.js
@@ -1,12 +1,12 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const HeaderArea = styled.div`
     height: 60px;
-    background-color: #FFF;
-    border-bottom: 1px solid #CCC;
+    background-color: #fff;
+    border-bottom: 1px solid #ccc;
 
     .container {
-        max-width: 1200px;
+        max-width:1200px;
         margin: 0 auto;
         display: flex;
     }
@@ -20,60 +20,54 @@ export const HeaderArea = styled.div`
         display: flex;
         align-items: center;
         height: 60px;
-
-        .logo-1,
-        .logo-2,
-        .logo-3 {
+    
+        .logo-1, .logo-2, .logo-3{
             font-size: 27px;
             font-weight: bold;
         }
 
-        .logo-1 { color: #F00; }
-        .logo-2 { color: #0F0; }
-        .logo-3 { color: #00F; }
+        .logo-1{ color: #F00;}
+        .logo-2{ color: #0F0;}
+        .logo-3{ color: #00F;}
     }
 
-    nav {
-        padding-top: 10px;
-        padding-bottom: 10px;
-
-        ul, li {
+    nav{
+        padding: 10px 0;
+    
+        ul, li{
             margin: 0;
             padding: 0;
             list-style: none;
         }
 
-        ul {
+        ul{
             display: flex;
             align-items: center;
             height: 40px;
         }
 
-        li {
-            margin-left: 20px;
-            margin-right: 20px;
+        li{
+            margin: 0 20px;
 
-            a {
+            a, button{
                 color: #000;
                 font-size: 14px;
-
-                &:hover {
+                
+                &:hover{
                     color: #999;
                 }
-
-                &.button {
+            
+                &.button{
                     background-color: #FF8100;
                     border-radius: 4px;
                     color: #FFF;
                     padding: 5px 10px;
                 }
 
-                &.button:hover {
+                &.button:hover{
                     background-color: #E57706;
                 }
             }
         }
     }
-
-   
 `
